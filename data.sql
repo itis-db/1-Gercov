ea32d9ae05e1a8f4eb729bffd156ee692c4211d8
Aleksandr <sasamrfn@gmail.com>
2023-10-23 21:09:54 +0300

    initial commit

--- data.sql
+++ data.sql
@@ -1,17 +1,48 @@
-    insert into activitytype(
-    	  activitytypeid
-        ,name
-        ,sysname
-    )
-    select d.*
-    from (
-    	values(1,'Программа','Program')
-        ,(2,'Подпрограмма','SubProgram')
-        ,(3,'Проект','Project')
-        ,(4,'Контракт','Contract')
-        ,(5,'КТ','Point')
-    ) d(activitytypeid, name, sysname)
-    where not exists(
-    	select 1 from activitytype t
-        where t.activitytypeid = d.activitytypeid
-    );
+-- Вставка типов мероприятий
+INSERT INTO ActivityType (ActivityTypeId, Name)
+VALUES (1, 'Concert'),
+       (2, 'Masterclass'),
+       (3, 'Exhibition');
+
+-- Вставка мероприятий
+INSERT INTO Activity (ActivityId, ParentId, ActivityTypeId, Name, AreaId)
+VALUES (1, NULL, 1, 'Linkin Park Concert', 1),
+       (2, 1, 2, 'Street Art Workshop', 2),
+       (3, 1, 2, 'Sculpture Workshop', 3),
+       (4, 2, 1, 'Neffex Concert', 1),
+       (5, 2, 3, 'Van Gogh Exhibition', 3),
+       (6, NULL, 3, 'Modern Art Exhibition', 2);
+
+-- Вставка программ мероприятий
+INSERT INTO Program (ActivityId)
+VALUES (1),
+       (2),
+       (3),
+       (6);
+
+-- Вставка подпрограмм мероприятий
+INSERT INTO SubProgram (ActivityId)
+VALUES (4),
+       (5);
+
+-- Вставка проектов
+INSERT INTO Project (ActivityId)
+VALUES (2),
+       (3),
+       (4),
+       (5);
+
+-- Вставка контрактов
+INSERT INTO Contract (ActivityId, AreaId)
+VALUES (3, 1),
+       (4, 2),
+       (6, 3);
+
+-- Вставка точек мероприятий
+INSERT INTO Point (ActivityId)
+VALUES (1),
+       (2),
+       (3),
+       (4),
+       (5),
+       (6);
--- tables.sql
+++ tables.sql
@@ -1,11 +1,72 @@
+-- CreateTable
+CREATE TABLE "activity" (
+    "activityid" INTEGER NOT NULL,
+    "parentid" INTEGER,
+    "activitytypeid" INTEGER,
+    "name" VARCHAR(255),
+    "areaid" INTEGER,
 
-create table if not exists activitytype
-(
-    activitytypeid integer not null
-        constraint pk_activitytype
-            primary key,
-    name           text    not null,
-    sysname        text    not null
+    CONSTRAINT "activity_pkey" PRIMARY KEY ("activityid")
 );
 
--- 
+-- CreateTable
+CREATE TABLE "activitytype" (
+    "activitytypeid" INTEGER NOT NULL,
+    "name" VARCHAR(255),
+
+    CONSTRAINT "activitytype_pkey" PRIMARY KEY ("activitytypeid")
+);
+
+-- CreateTable
+CREATE TABLE "contract" (
+    "activityid" INTEGER NOT NULL,
+    "areaid" INTEGER,
+
+    CONSTRAINT "contract_pkey" PRIMARY KEY ("activityid")
+);
+
+-- CreateTable
+CREATE TABLE "point" (
+    "activityid" INTEGER NOT NULL,
+
+    CONSTRAINT "point_pkey" PRIMARY KEY ("activityid")
+);
+
+-- CreateTable
+CREATE TABLE "program" (
+    "activityid" INTEGER NOT NULL,
+
+    CONSTRAINT "program_pkey" PRIMARY KEY ("activityid")
+);
+
+-- CreateTable
+CREATE TABLE "project" (
+    "activityid" INTEGER NOT NULL,
+
+    CONSTRAINT "project_pkey" PRIMARY KEY ("activityid")
+);
+
+-- CreateTable
+CREATE TABLE "subprogram" (
+    "activityid" INTEGER NOT NULL,
+
+    CONSTRAINT "subprogram_pkey" PRIMARY KEY ("activityid")
+);
+
+-- AddForeignKey
+ALTER TABLE "activity" ADD CONSTRAINT "activity_activitytypeid_fkey" FOREIGN KEY ("activitytypeid") REFERENCES "activitytype"("activitytypeid") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "contract" ADD CONSTRAINT "contract_activityid_fkey" FOREIGN KEY ("activityid") REFERENCES "activity"("activityid") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "point" ADD CONSTRAINT "point_activityid_fkey" FOREIGN KEY ("activityid") REFERENCES "activity"("activityid") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "program" ADD CONSTRAINT "program_activityid_fkey" FOREIGN KEY ("activityid") REFERENCES "activity"("activityid") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "project" ADD CONSTRAINT "project_activityid_fkey" FOREIGN KEY ("activityid") REFERENCES "activity"("activityid") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "subprogram" ADD CONSTRAINT "subprogram_activityid_fkey" FOREIGN KEY ("activityid") REFERENCES "activity"("activityid") ON DELETE NO ACTION ON UPDATE NO ACTION;
